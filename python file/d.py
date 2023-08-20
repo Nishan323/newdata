42a2d791d742f59e01bb80c4180f9e0aa74d4fdc
NkumarRai <mr.raiali323@gmail.com>
2023-08-20 15:19:51 +0100

    skjlaskjf

--- python file/d.py
+++ python file/d.py
@@ -35,3 +35,21 @@ new_number = integer_number + float_number
 # display new value and resulting data type
 print("Value:",new_number)
 print("Data Type:",type(new_number))
+
+
+
+
+num_string = '12'
+num_integer = 23
+
+print("Data type of num_string before Type Casting:",type(num_string))
+
+# explicit type conversion
+num_string = int(num_string)
+
+print("Data type of num_string after Type Casting:",type(num_string))
+
+num_sum = num_integer + num_string
+
+print("Sum:",num_sum)
+print("Data type of num_sum:",type(num_sum))
